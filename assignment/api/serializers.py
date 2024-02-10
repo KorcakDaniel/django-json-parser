19ac9b053bad623bdc6c9bba2dfcec0bedbf980a
divine-within <13299392+divine-yume@users.noreply.github.com>
2024-02-10 18:09:53 +0100

    Update: update existing items

--- assignment/api/serializers.py
+++ assignment/api/serializers.py
@@ -16,24 +16,32 @@ better and probably desired."""
 
 
 class AttributeNameSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = AttributeName
         fields = ("id", "name", "code", "display")
 
 
 class AttributeValueSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = AttributeValue
         fields = ("id", "value")
 
 
 class AttributeSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = Attribute
         fields = ("id", "attribute_name", "attribute_value")
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = Product
         fields = (
@@ -48,24 +56,32 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class ProductAttributesSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = ProductAttributes
         fields = ("id", "attribute", "product")
 
 
 class ImageSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = Image
         fields = ("id", "name", "image")
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = ProductImage
         fields = ("id", "name", "image", "product")
 
 
 class CatalogSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = Catalog
         fields = ("id", "name", "image", "products", "attributes")
--- assignment/api/views.py
+++ assignment/api/views.py
@@ -1,29 +1,52 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import JSONParser
 from django.apps import apps
 
-from .serializers import *
+from .serializers import (
+    AttributeSerializer,
+    AttributeNameSerializer,
+    AttributeValueSerializer,
+    ProductSerializer,
+    ProductAttributesSerializer,
+    ImageSerializer,
+    ProductImageSerializer,
+    CatalogSerializer,
+)
 
 
 @api_view(["POST"])
 @parser_classes([JSONParser])
 def importData(request):
     errors = []
+    data = []
     for item in request.data:
         model_name = list(item.keys())[0]
+
         try:
             model = apps.get_model("api", model_name)
         except LookupError:
             return Response({"message": f"{model_name} is not a valid model."})
+
         serializer_ref = globals().get(f"{model_name}Serializer")
-        serializer = serializer_ref(data=item[model_name])
+        id = item[model_name].get("id")
+
+        try:
+            existing_instance = model.objects.get(id=id)
+            serializer = serializer_ref(
+                instance=existing_instance, data=item[model_name]
+            )
+        except ObjectDoesNotExist:
+            serializer = serializer_ref(data=item[model_name])
+
         if serializer.is_valid():
-            model_instance = model(**serializer.validated_data)
-            model_instance.save()
+            serializer.save()
+            data.append(serializer.data)
         else:
             errors.append({"error": f"{model_name} - {serializer.errors}"})
-    return Response({"received data": request.data, "errors": errors})
+
+    return Response({"received data": data, "errors": errors})
 
 
 @api_view(["GET"])
